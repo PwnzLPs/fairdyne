7c40bb72aa226f90c6ceacfcf2b86da9dbec938a
PwnzLPs <pwnzlps@gmail.com>
2017-03-21 17:39:44 -0400

    Update input.js

--- js/input.js
+++ js/input.js
@@ -1,6 +1,6 @@
 key_is_down = {};
 
-// Last update: 3/20/17 - Updated Key Inputs
+// Last update: 3/21/17 - Updated Key Inputs
 function handleKeyInput(key, dir) {
 
 	if (!game_started) return false;
